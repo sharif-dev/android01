f60ebf4a4c8ae86d9c65be01df5c877cbebd0fe1
Reza Soltani <rsoltani97@gmail.com>
2019-02-24 20:54:54 +0330

    Add checkstyle file

--- app/src/main/java/edu/sharif/prj01/Counter.java
+++ app/src/main/java/edu/sharif/prj01/Counter.java
@@ -3,15 +3,15 @@ package edu.sharif.prj01;
 public class Counter {
     protected long count = 0;
 
-    public void doWork(){
-        for (int i = 0; i<100000;i++){
+    public void doWork() {
+        for (int i = 0; i < 100000; i++) {
             this.count = this.count + 1;
         }
     }
 
-    public void safeDoWork(){ // preventing race condition with following two synchronized blocks
-        synchronized(this){ // you can put in parenthesis any object that you want to put a lock for the thread (that you want to work lonely) on it until it's work done
-            for (int i = 0; i<100000;i++){
+    public void safeDoWork() { // preventing race condition with following two synchronized blocks
+        synchronized (this) { // you can put in parenthesis any object that you want to put a lock for the thread (that you want to work lonely) on it until it's work done
+            for (int i = 0; i < 100000; i++) {
                 this.count = this.count + 1;
             }
         }
